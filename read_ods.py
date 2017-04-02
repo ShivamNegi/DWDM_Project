04c99ce2fcdc4a7b06f1836bda4d084b4b1bb7fc
ShivamNegi <shivamnegi2019@gmail.com>
2017-04-02 21:01:31 +0530

    Preprocessing more

--- read_ods.py
+++ read_ods.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 filename = raw_input("enter file location:")
 data = pyod.get_data(filename)
+new_data = OrderedDict()
 
 sheets = [
     "North_East",
@@ -20,7 +21,52 @@ sheets = [
     "Northern_Ireland",
     "3yr_average"]
 
-for sheet in sheets:
-    data[sheet] = data[sheet][7:]
+description = [
+    "Protein",
+    "Vitamin D",
+    "Thiamin",
+    "Riboflavin",
+    "Vitamin B6",
+    "Vitamin C",
+    "Calcium",
+    "Magnesium",
+    "Iron",
+    "Zinc"]
 
-pyod.write_data("out1.ods", data)
+def preprocess():
+    for sheet in sheets:
+        data[sheet] = data[sheet][7:]
+
+def preprocess_remove_citation_rural():
+    for sheet in sheets:
+        data[sheet] = data[sheet][:-8]
+
+def preprocess_remove_citation_urban():
+    for sheet in sheets:
+        data[sheet] = data[sheet][:-13]
+
+def preprocess_remove_extra_params_():
+    for sheet in sheets:
+        data[sheet] = data[sheet][:-28]
+
+def preprocess_description():
+    for sheet in sheets:
+        k = data[sheet]
+        k_new = []
+        for i in range(len(k)):
+            if i < 3:
+                k_new.append(k[i])
+                continue
+            if k[i][1] in description:
+                k_new.append(k[i])
+        new_data[sheet] = k_new
+
+
+def write_data():
+    filename = raw_input("Enter file to write to:")
+    pyod.write_data(filename, data)
+
+
+def write_newdata():
+    filename = raw_input("Enter file to write to:")
+    pyod.write_data(filename, new_data)

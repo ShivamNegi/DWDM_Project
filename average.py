1e834324e0cb3ef3c7d7d3ff9add08242b5de7cd
ShivamNegi <shivamnegi2019@gmail.com>
2017-04-05 20:17:12 +0530

    Adding class to final.ods

--- average.py
+++ average.py
@@ -30,10 +30,34 @@ def adding_accvalues(data):
     return data
 
 
+def adding_class(data):
+    new_data = OrderedDict()
+    for sheet in sheets:
+        k_new = []
+        for i in range(2, 12):
+            k_new = []
+            for k in range(4, 18):
+                if data[sheet][i][-1] > data[sheet][i][k]:
+                    data[sheet][i].insert("N", i + 1)
+                else:
+                    data[sheet][i].append("Y")
+    return data
+
+
 def write_data(data):
     filename = raw_input("Enter file to write to:")
     pyod.write_data(filename, data)
 
+
+def div_10(data):
+    for sheet in sheets:
+        for i in range(2, 12):
+            for k in range(4, 18):
+                try:
+                    data[sheet][i][k] = data[sheet][i][k] / 10
+                except:
+                    print(k)
+    return data
 
 def main(data, new_data):
     for sheet in sheets:
@@ -57,6 +81,8 @@ if __name__ == '__main__':
     filename = raw_input("Enter inside file name:")
     data = pyod.get_data(filename)
     new_data = OrderedDict()
+    data = div_10(data)
     new_data = main(data, new_data)
     new_data = adding_accvalues(new_data)
+    new_data = adding_class(new_data)
     write_data(new_data)
